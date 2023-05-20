26f968f79f86b05ab57d1ab435f884d40448917d
Hisham Galal <galal@unhcr.org>
2023-05-20 12:26:22 -0500

    docs

---
+++ R/countries.R
@@ -0,0 +1,17 @@
+#' Countries
+#'
+#' Country codes, names, UN major areas and UNHCR regional bureaux/operations.
+#'
+#' @format A tibble.
+#' \describe{
+#'  \item{iso_code}{ISO3 Country code.}
+#'  \item{unhcr_code}{UNHCR Country code.}
+#'  \item{name}{UNSD Name.}
+#'  \item{unhcr_region}{UNHCR Region name.}
+#'  \item{unsd_region}{UNSD Region name.}
+#'  \item{unsd_subregion}{UNSD Sub-region name.}
+#'  \item{unsd_imregion}{SDG Region name.}
+#'  \item{sdg_region}{SDG Region name.}
+#' }
+#' @source Table 22 annexed to the Global Trends 2020 report.
+"countries"
