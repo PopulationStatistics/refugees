e33b9d6394da44f056e7522eef367928315ee7c5
Hisham Galal <galal@unhcr.org>
2023-05-28 13:09:16 -0500

    clean-up countries.R

--- data-raw/countries.R
+++ data-raw/countries.R
@@ -5,25 +5,27 @@ unhcr_by_region <-
   jsonlite::fromJSON("https://api.unhcr.org/population/v1/regions")$items |>
   transmute(unhcr_region = name,
             data = map(id, \(x) jsonlite::fromJSON(glue::glue("https://api.unhcr.org/population/v1/countries?unhcr_region={x}"))$items)) |>
-  unnest(data) |>
-  select(iso_code = iso, unhcr_code = code, name, unhcr_region)
+  unnest(data)
 
 unhcr_no_region <-
   anti_join(jsonlite::fromJSON(glue::glue("https://api.unhcr.org/population/v1/countries"))$items,
-            unhcr_by_region,
-            by = c(code = "unhcr_code")) |>
-  transmute(iso_code = case_when(code == "SGS" ~ "SGS",
-                                 # FIXME: these are actually codes for machine-readable passports.
-                                 # Not part of the ISO standard. Should we keep them?
-                                 code == "STA" ~ "XXA",
-                                 code == "UKN" ~ "XXX"),
-            unhcr_code = code,
-            name = if_else(unhcr_code == "KOS", "Kosovo", name),
-            unhcr_region = case_when(code == "SGS" ~ "Europe",
-                                     code == "KOS" ~ "Europe",
-                                     code == "TIB" ~ "Asia and the Pacific"))
+            unhcr_by_region, by = "code")
 
-unhcr <- bind_rows(unhcr_by_region, unhcr_no_region)
+corrections <-
+  tribble(~code,  ~iso, ~name,                                          ~unhcr_region,
+          "SGS", "SGS", "South Georgia and the South Sandwich Islands", "Europe",
+          "KOS",    NA, "Kosovo",                                       "Europe",
+          "TIB",    NA, "Tibetan",                                      "Asia and the Pacific",
+          # FIXME: the next two ISO codes are for machine-readable passports, not countries.
+          # Should we keep them? The first appears in the API but the second doesn't.
+          "STA", "XXA", "Stateless",                                    NA,
+          "UKN", "XXX", "Unknown",                                      NA)
+
+unhcr <-
+  bind_rows(unhcr_by_region, unhcr_no_region) |>
+  rows_update(corrections, by = "code") |>
+  select(iso_code = iso, unhcr_code = code, name, unhcr_region) |>
+  arrange(name)
 
 m49 <-
   session("https://unstats.un.org/unsd/methodology/m49/overview") |>
@@ -34,6 +36,6 @@ m49 <-
          unsd_subregion = X6,
          unsd_imregion = X8)
 
-countries <- left_join(unhcr, m49) |> as_tibble() |> arrange(name)
+countries <- left_join(unhcr, m49)
 
 usethis::use_data(countries, overwrite = TRUE)
