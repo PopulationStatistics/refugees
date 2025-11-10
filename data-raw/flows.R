68f13991401e35d81752898040e6e0c33f4a20a5
JaKreu <109583574+JaKreu@users.noreply.github.com>
2025-11-10 15:46:58 +0100

    mid-year 2025 update

--- data-raw/flows.R
+++ data-raw/flows.R
@@ -1,11 +1,23 @@
 library(tidyverse)
 library(readxl)
-library(httr)
+library(curl)
+
 
 tmpf <- fs::file_temp(ext = "xlsx")
 
-GET("https://www.unhcr.org/refugee-statistics/insights/data/UNHCR_Flow_Data.xlsx",
-    write_disk(tmpf))
+h <- curl::new_handle()
+
+curl::handle_setheaders(
+  h,
+  "User-Agent"      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/126.0 Safari/537.36 R/curl",
+  "Referer"         = "https://www.unhcr.org/refugee-statistics/",
+  "Accept"          = "text/html,application/xhtml+xml,application/xml;q=0.9,application/octet-stream,*/*;q=0.8",
+  "Accept-Language" = "en-US,en;q=0.9",
+  "Connection"      = "keep-alive"
+)
+
+
+curl::curl_download("https://www.unhcr.org/refugee-statistics/insights/data/UNHCR_Flow_Data.xlsx", tmpf, handle = h, mode = "wb")
 
 flows <-
   read_excel(tmpf, sheet = "DATA") |>
